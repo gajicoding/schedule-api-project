2c09a1e4bd2dfa0982d700f01e0daa1459d08e09
huouvcti <huouvcti@gamil.com>
2025-05-21 15:41:21 +0900

    chore: 프로젝트 구성

---
+++ src/main/java/com/github/gajicoding/schedule_api_project/ScheduleApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.gajicoding.schedule_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gajicoding/schedule_api_project/ScheduleApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gajicoding.schedule_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
